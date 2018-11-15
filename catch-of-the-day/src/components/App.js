69cd025814ab4096f1c20bdef8dce5b7c42b38c5
mhuelfen <huelfe@gmx.de>
2018-11-15 16:39:50 +0100

    Format with prettier.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,45 +1,50 @@
-import React from 'react';
-import Header from './Header';
-import Inventory from './Inventory';
-import Order from './Order';
-import sampleFishes from '../sample-fishes';
-
+import React from "react";
+import Header from "./Header";
+import Inventory from "./Inventory";
+import Order from "./Order";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 class App extends React.Component {
-
   state = {
     fishes: {},
     order: {}
   };
 
-  addFish = (fish) => {
+  addFish = fish => {
     // 1. copy existing state, never modify directly
-    const fishes = {...this.state.fishes}
+    const fishes = { ...this.state.fishes };
     // 2. add new fish
-    fishes[`fish${Date.now()}`] = fish
+    fishes[`fish${Date.now()}`] = fish;
     // 3. set new state
     this.setState({
       fishes
     });
 
-    console.log("NEW FISH added")
+    console.log("NEW FISH added");
   };
 
   loadSampleFishes = () => {
-    this.setState({fishes: sampleFishes})
-  }
+    this.setState({ fishes: sampleFishes });
+  };
 
   render() {
-
     return (
-        <div className={"catch-of-the-day"}>
-          <div className="menu">
-            <Header tagline='Otters Rock'> </Header>
-          </div>
-          <Order/>
-          <Inventory addFish={this.addFish}
-                     loadSampleFishes={this.loadSampleFishes}/>
+      <div className={"catch-of-the-day"}>
+        <div className="menu">
+          <Header tagline="Otters Rock"> </Header>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
-    )
+        <Order />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
+      </div>
+    );
   }
 }
 
